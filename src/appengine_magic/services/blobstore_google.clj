bd7d43951a5f5bed7e7de8e652b6adde38adf5d5
Carl Schroeder <schroeder.carl.j@gmail.com>
2012-03-22 12:10:02 -0400

    debug messages and get_uploads

--- src/appengine_magic/services/blobstore_google.clj
+++ src/appengine_magic/services/blobstore_google.clj
@@ -1,6 +1,17 @@
 (in-ns 'appengine-magic.services.blobstore)
 
 
+(defn get-uploads [ring-request-map]
+  (let [^:HttpServletRequest request (:request ring-request-map)
+			raw-uploaded-blobs (slurp (.getInputStream request))
+        uploaded-blobs (read-string raw-uploaded-blobs)
+        processed-blobs (reduce (fn [acc [upload-name blob-key-str]]
+      									 	(assoc acc upload-name (BlobKey. blob-key-str)))
+                                {} uploaded-blobs)]
+    processed-blobs))
+
+;    (into {} (.getUploads (get-blobstore-service) request))))
+
 (defn uploaded-blobs [ring-request-map]
   (let [^:HttpServletRequest request (:request ring-request-map)]
     (into {} (.getUploadedBlobs (get-blobstore-service) request))))
--- src/appengine_magic/services/blobstore_local.clj
+++ src/appengine_magic/services/blobstore_local.clj
@@ -1,6 +1,17 @@
 (in-ns 'appengine-magic.services.blobstore)
 
 
+(defn get-uploads [ring-request-map]
+  (let [^:HttpServletRequest request (:request ring-request-map)
+        raw-uploaded-blobs (slurp (.getInputStream request))
+        uploaded-blobs (read-string raw-uploaded-blobs)
+        processed-blobs (reduce (fn [acc [upload-name blob-key-str]]
+;		(.out System "get-uploads request: %s" uploaded-blobs)
+                                  (assoc acc upload-name (BlobKey. blob-key-str)))
+                                {}
+                                uploaded-blobs)]
+    processed-blobs))
+
 (defn uploaded-blobs [ring-request-map]
   (let [^:HttpServletRequest request (:request ring-request-map)
         raw-uploaded-blobs (slurp (.getInputStream request))
