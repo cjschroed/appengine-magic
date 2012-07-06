da184b6cf8fbe713788181ac40f54a916e1a5479
Carl Schroeder <schroeder.carl.j@gmail.com>
2012-07-06 11:40:40 -0700

    changed version to 1.7.0 for SDK

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.cjschroed/appengine-magic "0.5.0.3"
+(defproject org.clojars.cjschroed/appengine-magic "0.5.0.4"
   :description "Google App Engine library for Clojure."
   :min-lein-version "1.7.0"
   :repositories {"releases" "http://appengine-magic-mvn.googlecode.com/svn/releases/"
@@ -18,12 +18,12 @@
                  [taglibs/standard "1.1.2"] ; repackaged-appengine-jakarta-standard-1.1.2.jar
                  [commons-el "1.0"]
                  ;; main App Engine libraries
-                 [com.google.appengine/appengine-api-1.0-sdk "1.6.3.1"]
-                 [com.google.appengine/appengine-api-labs "1.6.3.1"]
-                 [com.google.appengine/appengine-api-stubs "1.6.3.1"]
-                 [com.google.appengine/appengine-local-runtime "1.6.3.1"]
-                 [com.google.appengine/appengine-local-runtime-shared "1.6.3.1"]
-                 [com.google.appengine/appengine-testing "1.6.3.1"]
-                 [com.google.appengine/appengine-tools-api "1.6.3.1"]]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.7.0"]
+                 [com.google.appengine/appengine-api-labs "1.7.0"]
+                 [com.google.appengine/appengine-api-stubs "1.7.0"]
+                 [com.google.appengine/appengine-local-runtime "1.7.0"]
+                 [com.google.appengine/appengine-local-runtime-shared "1.7.0"]
+                 [com.google.appengine/appengine-testing "1.7.0"]
+                 [com.google.appengine/appengine-tools-api "1.7.0"]]
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
                      [swank-clojure "1.4.0"]])
